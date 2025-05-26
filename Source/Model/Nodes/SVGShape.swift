135c4ff6f17f5c528fd53a20b81196c225a3f163
Paco Wong <paco@goodnotesapp.com>
2025-05-27 00:23:33 +0800

    Add members to store marker start and marker end in SVGShape

--- Source/Model/Nodes/SVGShape.swift
+++ Source/Model/Nodes/SVGShape.swift
@@ -10,14 +10,20 @@ public class SVGShape: SVGNode {
 #if os(WASI) || os(Linux)
     public var fill: SVGPaint?
     public var stroke: SVGStroke?
+    public var markerStart: String?
+    public var markerEnd: String?
 #else
     @Published public var fill: SVGPaint?
     @Published public var stroke: SVGStroke?
+    @Published public var markerStart: String?
+    @Published public var markerEnd: String?
 #endif
 
     override func serialize(_ serializer: Serializer) {
         fill?.serialize(key: "fill", serializer: serializer)
         serializer.add("stroke", stroke)
+        serializer.add("marker-start", markerStart)
+        serializer.add("marker-end", markerEnd)
         super.serialize(serializer)
     }
 }
