17265e2d369c4731fe8adefb2a04d1f7a03f427d
Paco Wong <paco@goodnotesapp.com>
2025-05-27 00:24:10 +0800

    Add new parser

--- Source/Parser/SVG/Elements/SVGStructureParsers.swift
+++ Source/Parser/SVG/Elements/SVGStructureParsers.swift
@@ -119,3 +119,151 @@ class SVGUseParser: SVGBaseElementParser {
         return useNode
     }
 }
+
+class SVGVDefParser: SVGGroupParser {
+    override func doParse(context: SVGNodeContext, delegate: (XMLElement) -> SVGNode?) -> SVGNode? {
+        return SVGGroup(contents: parseContents(context: context, delegate: delegate))
+    }
+
+    func parseContents(context: SVGNodeContext, delegate: (XMLElement) -> SVGNode?) -> [SVGNode] {
+        return context.element.contents
+            .compactMap { $0 as? XMLElement }
+            .compactMap { delegate($0) }
+    }
+}
+
+class SVGMarkerParser: SVGBaseElementParser {
+
+    override func doParse(context: SVGNodeContext, delegate: (XMLElement) -> SVGNode?) -> SVGNode? {
+        let attributes = context.properties
+
+        let markerWidth = Self.parseDimension(attributes, "markerWidth") ?? SVGLength(percent: 100)
+        let markerHeight = Self.parseDimension(attributes, "markerHeight") ?? SVGLength(percent: 100)
+        let markerUnits = Self.parseMarkerUnits(attributes["markerUnits"])
+        let orient = Self.parseOrient(attributes["orient"])
+        let viewBox = Self.parseViewBox(attributes, context: context)
+        let par = Self.parsePreserveAspectRatio(string: attributes["preserveAspectRatio"], context: context)
+        let refX = Self.parseRefMagnitude(attributes["refX"])
+        let refY = Self.parseRefMagnitude(attributes["refY"])
+        return SVGMarker(markerHeight: markerHeight, markerUnits: markerUnits, markerWidth: markerWidth, orient: orient, preserveAspectRatio: par, refX: refX, refY: refY, viewBox: viewBox, contents: parseContents(context: context, delegate: delegate))
+    }
+
+    func parseContents(context: SVGNodeContext, delegate: (XMLElement) -> SVGNode?) -> [SVGNode] {
+        return context.element.contents
+            .compactMap { $0 as? XMLElement }
+            .compactMap { delegate($0) }
+    }
+
+    static func parseDimension(_ attributes: [String: String], _ key: String) -> SVGLength? {
+        guard let string = attributes[key] else {
+            return .none
+        }
+        if string.hasSuffix("%"), let value = Double(string.dropLast()) {
+            return SVGLength(percent: CGFloat(value))
+        }
+        if let value = Double(string) {
+            return SVGLength(pixels: CGFloat(value))
+        }
+        return .none
+    }
+
+    static func parseMarkerUnits(_ string: String?) -> SVGMarker.MarkerUnits {
+        if let anchor = string {
+            if anchor == "userSpaceOnUse" {
+                return .userSpaceOnUse
+            } else if anchor == "strokeWidth" {
+                return .strokeWidth
+            }
+        }
+        return .strokeWidth
+    }
+
+    static func parseOrient(_ value: String?) -> SVGMarker.Orient {
+        guard let value else {
+            return .angle(0)
+        }
+        if value == "auto" {
+            return .auto
+        } else if value == "auto-start-reverse" {
+            return .autoStartReverse
+        } else if let angle = Float(value) {
+            return .angle(angle)
+        } else {
+            return .angle(0)
+        }
+    }
+
+    static func parseRefMagnitude(_ value: String?) -> SVGMarker.RefMagnitude {
+        guard let value else {
+            return .coordinate(0)
+        }
+        if value == "right" {
+            return .right
+        } else if value == "left" {
+            return .left
+        } else if value == "center" {
+            return .center
+        } else if let magnitude = Double(value) {
+            return .coordinate(magnitude)
+        } else {
+            return .coordinate(0)
+        }
+    }
+
+    static func parsePreserveAspectRatio(string: String?, context: SVGContext) -> SVGPreserveAspectRatio {
+        if let contentModeString = string {
+            let strings = contentModeString.components(separatedBy: CharacterSet(charactersIn: " "))
+            if strings.count == 1 { // none
+                return SVGPreserveAspectRatio(scaling: parseScaling(strings[0]))
+            }
+            guard strings.count == 2 else {
+                context.log(message: "Invalid content mode \(contentModeString)")
+                return SVGPreserveAspectRatio()
+            }
+
+            let alignString = strings[0]
+            var xAlign = alignString.prefix(4).lowercased()
+            xAlign.remove(at: xAlign.startIndex)
+            let xAligningMode = parseAlign(xAlign)
+
+            var yAlign = alignString.suffix(4).lowercased()
+            yAlign.remove(at: yAlign.startIndex)
+            let yAligningMode = parseAlign(yAlign)
+
+            let scalingMode = parseScaling(strings[1])
+            return SVGPreserveAspectRatio(scaling: scalingMode, xAlign: xAligningMode, yAlign: yAligningMode)
+        }
+        return SVGPreserveAspectRatio()
+    }
+
+    static func parseAlign(_ string: String) -> SVGPreserveAspectRatio.Align {
+        switch string {
+            case "min": return .min
+            case "max": return .max
+            default: return .mid
+        }
+    }
+
+    static func parseScaling(_ string: String) -> SVGPreserveAspectRatio.Scaling {
+        switch string {
+            case "meet": return .meet
+            case "slice": return .slice
+            default: return .none
+        }
+    }
+
+    static func parseViewBox(_ attributes: [String: String], context: SVGContext) -> CGRect? {
+        // TODO: temporary solution, all attributes should be case insensitive
+        if let string = attributes[ignoreCase: "viewBox"] {
+            let nums = string.components(separatedBy: .whitespaces)
+            if nums.count == 4,
+               let x = SVGLengthParser.xAxis.double(string: nums[0], context: context),
+               let y = SVGLengthParser.yAxis.double(string: nums[1], context: context),
+               let width = SVGLengthParser.xAxis.double(string: nums[2], context: context),
+               let height = SVGLengthParser.yAxis.double(string: nums[3], context: context) {
+                return CGRect(x: x, y: y, width: width, height: height)
+            }
+        }
+        return nil
+    }
+}
