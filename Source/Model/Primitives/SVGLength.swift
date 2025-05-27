7f9ba697625a25497d60062fc264dbf518f270de
Paco Wong <paco@goodnotesapp.com>
2025-05-27 15:16:52 +0800

    Add .zero

--- Source/Model/Primitives/SVGLength.swift
+++ Source/Model/Primitives/SVGLength.swift
@@ -16,6 +16,8 @@ public enum SVGLength {
     case percent(CGFloat)
     case pixels(CGFloat)
 
+    static let zero: SVGLength = .pixels(0)
+
     init(percent: CGFloat) {
         self = .percent(percent)
     }
