ada46ea5674beee653087b7da6d688cde931121a
khoi <git@khoi.io>
2025-05-27 16:32:18 +0700

    Restore the original logic of serialization

--- Source/Serialization/Serializations.swift
+++ Source/Serialization/Serializations.swift
@@ -47,7 +47,18 @@ extension Double: SerializableAtom {
 extension CGAffineTransform: SerializableAtom {
 
     func serialize() -> String {
-        return String(format: "[%.10f, %.10f, %.10f, %.10f, %.10f, %.10f]", a, b, c, d, tx, ty)
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 10
+        
+        let nums = [a, b, c, d, tx, ty]
+        
+        var result = ""
+        for num in nums {
+            result += formatter.string(for: num) ?? "n/a"
+            result += ", "
+        }
+        return "[\(result.dropLast(2))]"
     }
 }
 
